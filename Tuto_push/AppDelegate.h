11ba6c5e7f59f2726afd38fc6c4decb9e08c5a5f
ipodishima <marian.paul2@gmail.com>
2012-06-15 17:15:49 +0200

    premier commit avant envoi

---
+++ Tuto_push/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tuto_push
+//
+//  Created by Jérémy Lagrue on 13/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
